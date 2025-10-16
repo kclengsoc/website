93263dddf0209f7f44068c286a0c0a60fc276d3b
KCL Engineering Society <engineeringsoc.kcl@gmail.com>
2025-10-16 20:16:54 +0100

    Update event.js

--- src/_data/event.js
+++ src/_data/event.js
@@ -1,7 +1,7 @@
 // src/_data/events.js
 export default [
   {
-    title: "CAD and 3D printing workshop",
+    title: "CAD and 3D Printing Workshop",
     date: "2025-10-15",
     location: "General Classroom, Hodgkin Classroom",
     content: "Introductory class to CAD and 3D printing using TinkerCAD and Bambu printers.",
@@ -11,12 +11,12 @@ export default [
     title: "MATLAB Workshop",
     date: "2025-10-22",
     location: "To be announced",
-    content: "Workshop on MATLAB with KCL MATLAB ambassador"
+    content: "Workshop on MATLAB with KCL MATLAB ambassador."
   },
   {
-    title: "Cross-society networking event: Grow your ecosySTEM",
+    title: "Cross-Society Networking Event: Grow Your ecosySTEM",
     date: "2025-10-29",
-    location: "Engineering Quad, Rooms Q-126 & Q-1.47, in Strand Campus",
-    content: "Join us for an evening of connection, collaboration and activities as KCL STEM societies come together!",
+    location: "Engineering Quad, Rooms Q-126 & Q-1.47, Strand Campus",
+    content: "Join us for an evening of connection, collaboration, and activities as KCL STEM societies come together!"
   }
 ];
